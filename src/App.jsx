aa5396d90f4988db6ea615b762fcfe2930305f53
sam987-dek <225321092+sam987-dek@users.noreply.github.com>
2026-02-25 23:50:23 +0530

    Add Routes and Route components to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { HashRouter as Router } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom"
 import Home from './pages/Home'
 import RecipeDetail from './pages/RecipeDetail'
 
